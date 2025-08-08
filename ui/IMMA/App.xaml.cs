085b239711434674b2fa3ff9b079e7ee2c650831
sardelka9515 <sardelka9515@gmail.com>
2025-08-08 14:44:34 +0800

    ui

---
+++ ui/IMMA/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace IMMA
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = new Window(new AppShell());
+            window.Width = 800;
+            window.Height = 800;
+            return window;
+        }
+    }
+}
---
+++ ui/IMMA/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace IMMA
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
